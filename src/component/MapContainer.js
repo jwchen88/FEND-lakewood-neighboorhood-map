c7b43d73e72c148470d551be9baf1d74255eb582
Jiawen Chen <sophiachen1130@gmail.com>
2018-11-13 23:13:06 -0600

    Add Marker

--- src/component/MapContainer.js
+++ src/component/MapContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import {searchNearby} from '../utils/GoogleApi'
+import Sidebar from './Sidebar.js'
 
 export class MapContainer extends Component {
   constructor(props){
@@ -15,9 +16,9 @@ export class MapContainer extends Component {
   onReady(mapProps, map){
     const {google} = this.props;
     const opts = {
-      location: {lat:32.8132922, lng:-96.7521698},
-      radius: '1000',
-      type: ['food']
+      location: {lat:32.8124432, lng:-96.7514695},
+      radius: '500',
+      type: ['restaurant']
     }
     searchNearby(google, map, opts)
     .then((results, pagination) => {
@@ -30,21 +31,40 @@ export class MapContainer extends Component {
 
     })
   }
+
+  renderMarkers() {
+    if(!this.state.places){ return null; }
+    return this.state.places.map(place =>{
+      return <Marker
+        key={place.id}
+        name={place.id}
+        place={place}
+        position={place.geometry.location}
+      />
+    })
+  }
+
   render() {
     return (
       <div>
         <Map
           onReady={this.onReady.bind(this)}
           google={this.props.google}
-          visible={false}
+          initialCenter={{
+            lat: 32.8124432,
+            lng: -96.7514695
+          }}
+          zoom={15}
         >
-          {this.state.places.map(place => {
-            return (
-              <div key={place.id}>
-                {place.name}
-              </div>
-            )
-          })}
+
+          {this.renderMarkers()}
+
+          <Sidebar
+            title={'Restaurants'}
+            //onListItemClick={this.onMarkerClick.bind(this)}
+            places={this.state.places}
+          />
+
           {/*
             <Marker onClick={this.onMarkerClick}
             name={'Current location'} />
