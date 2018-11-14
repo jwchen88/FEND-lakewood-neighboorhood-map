6d14fcc7b655b5a5e0c2e1922e1ed41e673112c2
Jiawen Chen <sophiachen1130@gmail.com>
2018-11-14 15:53:01 -0600

    Add search to the list

--- src/component/ListPlaces.js
+++ src/component/ListPlaces.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Place from './Place.js'
+import escapeRegExp from 'escape-string-regexp'
 
 export class ListPlaces extends Component {
+  static PropTypes = {
+    places: PropTypes.array.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({query: query})
+  }
+
   render() {
+    let showingPlaces
+    if (this.state.query){
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingPlaces = this.props.places.filter((place) => match.test(place.name))
+    } else {
+      showingPlaces = this.props.places
+    }
+    
     return (
-      <div className="list-places-content">
-        {this.props.places.map(place => {
+      <div className="place-list">
+        <div className="search">
+          <input
+            className="search-restaurant"
+            type="text"
+            placeholder="Search"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        {showingPlaces.map(place => {
           return (
             <Place
               place={place}
-              onClick={this.props.onClick}
+              //onClick={this.props.onClick}
               key={place.id}
             />
           )
